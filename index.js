a74f1b8a0f8fa0c518254d507c10083a52843f01
Cici Chen <iccchen@bu.edu>
2021-06-11 08:52:12 -0400

    google cal api get events, but doesn't display to resources.html

--- index.js
+++ index.js
@@ -181,3 +181,115 @@ app.post('/signup', (req, res) => {
 app.listen(port, () => {
     console.log(`App started on port http://localhost:${port}...`);
 }); // currently hosting on localhost:3000
+
+
+
+
+
+// // CALENDAR
+// const {google} = require('googleapis');
+// require('dotenv').config();
+
+// // Provide the required configuration
+// const CREDENTIALS = JSON.parse(process.env.GOOGLE_API_CREDENTIALS);
+// const calendarId = process.env.CALENDAR_ID;
+
+// // Google calendar API settings
+// const SCOPES = 'https://www.googleapis.com/auth/calendar';
+// const calendar = google.calendar({version : "v3"});
+
+// const auth = new google.auth.JWT(
+//     CREDENTIALS.client_email,
+//     null,
+//     CREDENTIALS.private_key,
+//     SCOPES
+// );
+
+// // Your TIMEOFFSET Offset
+// const TIMEOFFSET = '+05:30';
+
+// // Get date-time string for calender
+// const dateTimeForCalander = () => {
+
+//     let date = new Date();
+
+//     let year = date.getFullYear();
+//     let month = date.getMonth() + 1;
+//     if (month < 10) {
+//         month = `0${month}`;
+//     }
+//     let day = date.getDate();
+//     if (day < 10) {
+//         day = `0${day}`;
+//     }
+//     let hour = date.getHours();
+//     if (hour < 10) {
+//         hour = `0${hour}`;
+//     }
+//     let minute = date.getMinutes();
+//     if (minute < 10) {
+//         minute = `0${minute}`;
+//     }
+
+//     let newDateTime = `${year}-${month}-${day}T${hour}:${minute}:00.000${TIMEOFFSET}`;
+
+//     let event = new Date(Date.parse(newDateTime));
+
+//     let startDate = event;
+//     // get events within 1 year = 8760 hours (end time) from the moment of access
+//     let endDate = new Date(new Date(startDate).setHours(startDate.getHours()+8760));
+
+//     return {
+//         'start': startDate,
+//         'end': endDate
+//     }
+// };
+
+// // Get all the events between two dates
+// const getEvents = async () => {
+//     let dateTime = dateTimeForCalander();
+//     let dateTimeStart = dateTime['start'];
+//     let dateTimeEnd = dateTime['end'];
+//     try {
+//         let response = await calendar.events.list({
+//             auth: auth,
+//             calendarId: calendarId,
+//             timeMin: dateTimeStart,
+//             timeMax: dateTimeEnd,
+//             timeZone: 'Asia/Kolkata'
+//         });
+    
+//         let items = response['data']['items'];
+//         return items;
+//     } catch (error) {
+//         console.log(`Error at getEvents --> ${error}`);
+//         return 0;
+//     }
+// };
+
+// var allEvents = {};
+// getEvents()
+//     .then((res) => {
+//         allEvents = res;
+//         console.log(res[0])
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+//     console.log(allEvents)
+// app.get('/resources.html', function(req,res) {
+//     res.render(__dirname+'/views/resources.html', {message: allEvents});
+//     // getEvents()
+//     //     .then((res) => {
+//     //         res.render(__dirname+'/views/resources.html', {message:res})
+//     //             .catch((err) => {
+//     //                 console.log(err);
+//     //             })
+//     //     });
+// });
+
+
+// app.listen(port, () => {
+//     console.log(`App started on port http://localhost:${port}...`);
+// }); // currently hosting on localhost:3000
